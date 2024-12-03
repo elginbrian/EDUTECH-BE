a30677176b81d32912eb113e4e803afa6d9a1243
Elgin Brian Wahyu Bramadhika <132267129+elginbrian@users.noreply.github.com>
2024-12-03 12:40:44 +0700

    change: a couple things

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,14 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Welcome to EDUTECH by BCC RAION RAION.';
+  getHello() {
+    return {
+      message: 'Welcome to EDUTECH by BCC RAION RAION',
+      teamMembers: [
+        { name: 'Pande Gede Natha Satvika', role: 'UI/UX' },
+        { name: 'Elgin Brian Wahyu Bramadhika', role: 'BE' },
+        { name: 'Anak Agung Aditya Wirayudha', role: 'FE' },
+      ],
+    };
   }
 }
